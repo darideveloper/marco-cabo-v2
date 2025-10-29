8e509dfb03eefae96cc76070ec4585424ccbcff2
Silvia <silviaislam.eera@gmail.com>
2025-10-30 01:28:14 +0600

    fix: rename CardVehicle to Vehicle card

--- src/components/molecules/VehicleSelect.tsx
+++ src/components/molecules/VehicleSelect.tsx
@@ -3,7 +3,8 @@ import clsx from "clsx"
 
 
 // components
-import CardVehicle from "./CardVehicle"
+import CardVehicle from "./VehicleCard"
+import { useState } from "react"
 //  props
 interface Props {
     className?: string
@@ -11,16 +12,38 @@ interface Props {
 
 
 export default function VehicleSelect({ className }: Props) {
+
+    const vehicles = [
+        {
+            id: 1,
+            title: "Luxury SUV",
+            numberOfGuests: 4,
+            image: "/images/Sprinter.webp"
+        },
+        {
+            id: 2,
+            title: "Executive Van",
+            numberOfGuests: 8,
+            image: "/images/SUB.webp"
+        },
+        {
+            id: 3,
+            title: "Luxury SUV",
+            numberOfGuests: 12,
+            image: "/images/Van.webp"
+        }
+    ]
+
+    const [selectedVehicle, setSelectedVehicle] = useState<number>(1)
+
     return (<div className={clsx(
         'grid grid-cols-1 md:grid-cols-3 gap-4',
         'mx-auto',
         'w-full',
         className
     )}>
-        <CardVehicle title="Luxury SUV" numberOfGuests={4} image="/images/Sprinter.webp" isSelected={true} />
-
-        <CardVehicle title="Executive Van" numberOfGuests={8} image="/images/SUB.webp" />
-
-        <CardVehicle title="Luxury SUV" numberOfGuests={12} image="/images/Van.webp" />
+        {vehicles.map((vehicle) => (
+            <CardVehicle key={vehicle.id} title={vehicle.title} numberOfGuests={vehicle.numberOfGuests} image={vehicle.image} isSelected={vehicle.id === selectedVehicle} />
+        ))}
     </div>)
 }
