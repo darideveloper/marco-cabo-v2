b9fe4e800e9163bb306bcfddd5bcabcc1f05081a
daridev <darideveloper@gmail.com>
2025-11-12 18:44:45 -0600

    feat (deploy): enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import react from '@astrojs/react';
 
 import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), showTailwindcssBreakpoint()]
+  integrations: [react(), showTailwindcssBreakpoint()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
