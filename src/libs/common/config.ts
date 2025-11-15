e3e0c58f00feea662f02c3442f9a99e8d42a5877
daridev <darideveloper@gmail.com>
2025-11-15 16:27:13 -0600

    fix (src/libs/common/config): read env variables as 'PUBLIC_'

--- src/libs/common/config.ts
+++ src/libs/common/config.ts
@@ -1,5 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL = import.meta.env.PUBLIC_BASE_URL
 export const API_URL = `${BASE_URL}/api`
 
-export const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY
-
+export const TOKEN_KEY = import.meta.env.PUBLIC_TOKEN_KEY
