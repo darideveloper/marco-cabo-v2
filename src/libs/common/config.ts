53697f846c6589c28cd2f7f64eb6045f206aaa42
daridev <darideveloper@gmail.com>
2025-11-15 13:46:54 -0600

    fix (libs/common/config): get api url and token from env variables

--- src/libs/common/config.ts
+++ src/libs/common/config.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://marco-cabo-testing.apps.darideveloper.com"
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 export const API_URL = `${BASE_URL}/api`
 
-export const TOKEN_KEY = "f2334c6f4c74cae6ab4f179ab8c0ecfb2b37ffb8"
+export const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY
 
