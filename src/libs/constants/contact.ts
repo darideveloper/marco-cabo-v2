adbaaf4c3874b86f8d0ab40acb72f65608e52741
daridev <darideveloper@gmail.com>
2025-11-25 15:45:04 -0600

    feat (seo): render metadata

---
+++ src/libs/constants/contact.ts
@@ -0,0 +1,15 @@
+export const socialLinks = [
+    {
+        iconName: 'facebook',
+        href: 'https://www.facebook.com/profile.php?id=100093181244966',
+        label: 'Facebook',
+    },
+    {
+        iconName: 'instagram',
+        href: 'https://www.instagram.com/profile.php?id=100093181244966',
+        label: 'Instagram',
+    },
+]
+
+export const email = 'info@marco-cabo.com'
+export const phone = '+52 624 176 4802'
