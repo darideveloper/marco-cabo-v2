f0278f285198c6e2448a9af16d16b93586b3a317
daridev <darideveloper@gmail.com>
2025-11-30 14:21:47 -0600

    feat (SEO): add google maps link

--- src/libs/constants/contact.ts
+++ src/libs/constants/contact.ts
@@ -13,3 +13,4 @@ export const socialLinks = [
 
 export const email = 'info@marco-cabo.com'
 export const phone = '+52 624 176 4802'
+export const googleMapsUrl = 'https://www.google.com/maps/place/Mar+Co.+CABO/@22.9335587,-109.8892175,12z/data=!3m1!4b1!4m6!3m5!1s0x49b5b7d282f25079:0xb5f28acf5a653a64!8m2!3d22.9335587!4d-109.8892174!16s%2Fg%2F11zkhv0rxy?entry=ttu&g_ep=EgoyMDI1MTEyMy4xIKXMDSoASAFQAw%3D%3D'
